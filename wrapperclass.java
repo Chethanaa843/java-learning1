ec42e389136c6c988ce51c94b9a955ecd57ee3be
Lohith K U <kuchethana@gmail.com>
2026-02-11 10:58:56 +0530

    completed

---
+++ wrapperclass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class wrapperclass{
+     public static void main(String[] args) {
+   Scanner scn=new Scanner(System.in);
+    int a=10;
+    Integer b=a;    //Autoboxing
+   
+    Integer c=10;
+    int d=c;
+    System.out.println("The value of a is variable: "+a);
+    System.out.println("The value of b is wrapper class: "+b);
+    System.out.println("The value of c is wrapper class: "+c);
+    System.out.println("The value of d is variable: "+d);
+    }
+}
+
